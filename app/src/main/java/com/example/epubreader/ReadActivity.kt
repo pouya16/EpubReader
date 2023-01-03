209dd837a92071315392973dd1f4f44e58256335
pouya <pouya16@gmail.com>
2023-01-03 07:20:57 -0800

    1401/10/13 initialize epub download and reader

---
+++ app/src/main/java/com/example/epubreader/ReadActivity.kt
@@ -0,0 +1,11 @@
+package com.example.epubreader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_read)
+    }
+}
